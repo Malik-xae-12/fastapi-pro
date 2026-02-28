0d551db675c847da5d5269d7db7eec77a3213615
Malik-xae-12 <az786muzaffar@gmail.com>
2026-02-28 20:51:44 +0530

    pydantic,header,endpoints

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI,Header
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -11,10 +12,43 @@ async def main():
 
 @app.get("/greet/{name}")
 async def greet(name:str):
-    return {"message":f"hello {name}"}
+    return {"message":f"hello {name}","age":f"{age}"}
 
 
-@app.get("/greet/{name}")
-async def greet(name:str):
-    return {"message":f"hello {name}"}    
+# pass Multiple Query Parameters
+@app.get("/greet")
+async def greet(name:Optional[str]="malik",age:Optional[int]=22):
+    return {"message":f"hello {name}","age":f"{age}"}    
+
+
+class BookCreateModel(BaseModel):
+    title : str
+    author : str
+    records : int
+
+
+
+@app.post('/create_book')
+async def create_book(book_data:BookCreateModel):
+    return {
+        "title":book_data.title,
+        "author":book_data.author,
+        "records" :book_data.records
+    }
+
+
+@app.get('/get_headers',status_code=201)
+async def get_headers(
+    accept:str=Header(None),
+    content_type:str = Header(None),
+    host:str = Header(None),
+    user_agent:str= Header(None)
+):
+    request_headers={}
+    request_headers["Accept"] = accept
+    request_headers['Content-Type']=content_type
+    request_headers['host'] = host
+    request_headers['User-Agent'] = user_agent
+    return request_headers
+
 
